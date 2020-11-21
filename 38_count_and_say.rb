d0e90381163214e7f36bcc4aa7bfb71d72938f56
Raghu Srinivasan <raghu.srinivasan@gmail.com>
2020-11-21 12:55:20 -0800

    Count and Say

--- 38_count_and_say.rb
+++ 38_count_and_say.rb
@@ -5,12 +5,15 @@
 # @return {String}
 def count_and_say(n)
     if n == 1
-        return '1' # base case
+        return '1' # base case for reursion
     end
 
     # recursively call the next lower value of n ie (n-1)
     array = count_and_say(n-1).to_s.split("")
 
+    # we count repetitions ie occurences and then
+    # form a string of the occurences + number
+    # ie 111 => 31(s) and so on
     number_of_repetitions = 1
     str = ''
     (0..array.length-1).each do |i|
