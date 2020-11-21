e93bc5435841b2bf675e78f1fa851ddce1f031b0
Raghu Srinivasan <raghu.srinivasan@gmail.com>
2020-11-20 17:31:11 -0800

    #49. Group Anagrams

---
+++ 49_group_anagrams.rb
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/group-anagrams/
+
+# @param {String[]} strs
+# @return {String[][]}
+def group_anagrams(strs)
+    
+    # set up an empty hash
+    anagrams = Hash.new 
+
+    strs.each do |word| # iterate through the given array of words
+
+        # arrange each word alphabetically ie sort it
+        sorted_word = word.split("").sort.join("") 
+        
+        # check if this sorted word is in our Hash/Dictionary
+        if anagrams[sorted_word].nil?
+            # if it isn't add it with 
+            # the key as the sorted word and the value as the original word
+            anagrams[sorted_word] = [word]
+        else
+            # if it in the hash, append the original word to the array
+            # that is the value for the same key
+            anagrams[sorted_word] << word
+        end
+    end
+    # at this point we have the anagrams variable populated with each key 
+    # being the sorted word and the value being an array of the orig words
+
+    # we don't need to output the keys, 
+    # just collect the values and return it
+    output = []
+    anagrams.each do |k,v|
+        output << v
+    end
+    return output
+end
